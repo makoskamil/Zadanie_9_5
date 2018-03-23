85dfe296f5d127145ddcea27892060be111224e2
Kamil Makoś <makos.kamil@o2.pl>
2018-03-23 08:56:18 +0100

    Mod_JS

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ var withButtonClass = document.getElementsByClassName('button');
     console.log(withButtonClass.length);
 
 for ( var k = 0 ; k < withButtonClass.length ; k++) {
-    console.log(withButtonClass);
+    console.log(withButtonClass[k]);
 }
