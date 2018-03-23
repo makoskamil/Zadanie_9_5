f2f3e8c691e02089cf2730c84b123baf1d324d0e
Kamil Makoś <makos.kamil@o2.pl>
2018-03-23 08:42:04 +0100

    Add_Js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var withButtonClass = document.getElementsByClassName('button');
+    console.log(withButtonClass.length);
+
+for ( var k = 0 ; k < withButtonClass.length ; k++) {
+    console.log(withButtonClass);
+}
